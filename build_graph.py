946ae27caa50bcc1467ec880c130066194901174
colinlee1999 <colinlee1999@gmail.com>
2016-05-05 10:58:18 +0800

    undirected graph included

--- build_graph.py
+++ build_graph.py
@@ -8,7 +8,31 @@ def get_coo_from_mat_v1(mat_file):
   mat = scipy.io.loadmat(mat_file)
   return mat['Problem'][0][0][2].tocoo()
 
-# for directed graph
+def get_directed_graph_from_coo(coo):
+  G = TNGraph.New()
+  for node_id in range(coo.shape[0]):
+    G.AddNode(node_id)
+  for edges_id in range(len(coo.row)):
+    G.AddEdge(int(coo.row[edges_id]), int(coo.col[edges_id])) 
+  return G
+
+def get_undirected_graph_from_coo(coo):
+  pass
+
+def get_undirected_graph_from_dblp_v1(dblp_file):
+  G = TUNGraph.New()
+  raw_data = open(dblp_file).read().split('\n')[0:-1]
+  for i in range(4, len(raw_data)):
+    temp = raw_data[i].split('\t')
+    src_id = int(temp[0])
+    dst_id = int(temp[1])
+    if not G.IsNode(src_id):
+      G.AddNode(src_id)
+    if not G.IsNode(dst_id):
+      G.AddNode(dst_id)
+    G.AddEdge(src_id, dst_id)
+  return G
+
 def get_directed_graph_from_p2p_v1(p2p_file):
   G = TNGraph.New()
   raw_data = open(p2p_file).read().split('\r\n')[0:-1]
@@ -22,6 +46,313 @@ def get_directed_graph_from_p2p_v1(p2p_file):
       G.AddNode(dst_id)
     G.AddEdge(src_id, dst_id)
   return G
+
+##################################
+# undirected
+
+def random_node_sample_graph_from_undirected(G, density):
+  new_G = TUNGraph.New()
+  for node_id in range(G.GetNodes()):
+    if random.random() < density:
+      new_G.AddNode(node_id)
+  
+  for edges in G.Edges():
+    src_id = edges.GetSrcNId()
+    dst_id = edges.GetDstNId()
+    if new_G.IsNode(src_id) and new_G.IsNode(dst_id):
+      new_G.AddEdge(src_id, dst_id)
+  
+  return new_G
+
+def random_edge_sample_graph_from_undirected(G, density):
+  new_G = TUNGraph.New()
+  for edges in G.Edges():
+    if random.random() < density:
+      src_id = edges.GetSrcNId()
+      dst_id = edges.GetDstNId()
+      if not new_G.IsNode(src_id):
+        new_G.AddNode(src_id)
+      if not new_G.IsNode(dst_id):
+        new_G.AddNode(dst_id)
+      new_G.AddEdge(src_id, dst_id)
+
+  return new_G
+
+def random_walking_sample_graph_from_undirected(G, density, prob_jump_to_start = 0.15, max_iter_without_found = 100):
+  start_id_list = []
+
+  new_G = TUNGraph.New()
+  num_nodes_new_G = 0
+  
+  num_nodes_G = G.GetNodes()
+  while num_nodes_new_G < num_nodes_G * density:
+    
+    # find the start point id
+    found = False
+    while not found:
+      start_id = G.GetRndNId()
+      if not start_id in start_id_list:
+        found = True
+    
+    # add new point to list
+    start_id_list.append(start_id)
+
+    # if not new_G.IsNode(start_id):
+    if not new_G.IsNode(start_id):
+      new_G.AddNode(start_id)
+      num_nodes_new_G = num_nodes_new_G + 1
+    
+    current_id = start_id
+    times_iter_without_found = 0
+    while num_nodes_new_G < num_nodes_G * density:
+      found = False
+
+      if random.random() < prob_jump_to_start:
+        current_id = start_id
+      
+      num_out_edges = sum(1 for x in G.GetNI(current_id).GetOutEdges())
+      num_iterated_edges = 0
+
+      last_id = current_id
+      for out_edge_id in G.GetNI(current_id).GetOutEdges():
+        if random.random() * (num_out_edges - num_iterated_edges) < 1:
+          current_id = out_edge_id
+          break;
+        num_iterated_edges = num_iterated_edges + 1
+
+      if not new_G.IsNode(current_id):
+        new_G.AddNode(current_id)
+        num_nodes_new_G = num_nodes_new_G + 1
+        found = True
+
+      if not new_G.IsEdge(last_id, current_id):
+        new_G.AddEdge(last_id, current_id)
+        found = True
+
+      if found:
+        times_iter_without_found = 0
+      else:
+        times_iter_without_found = times_iter_without_found + 1
+        if times_iter_without_found >= max_iter_without_found:
+          break
+
+  return new_G
+
+def forest_fire_sample_graph_from_undirected(G, density, p = 0.2):
+  id_list_buffer = []
+
+  new_G = TUNGraph.New()
+  num_nodes_G = G.GetNodes()
+
+  head = 0
+  tail = 0
+
+  while tail < num_nodes_G * density:
+
+    # find the start point id
+    found = False
+    while not found:
+      start_id = G.GetRndNId()
+      if not start_id in id_list_buffer:
+        found = True
+
+    id_list_buffer.append(start_id)
+    tail = tail + 1
+
+    new_G.AddNode(start_id)
+
+    while head<tail:
+      current_id = id_list_buffer[head]
+      x = geometric(p=p, size=1)
+
+      num_out_edges = sum(1 for x in G.GetNI(current_id).GetOutEdges())
+
+      old_tail = tail
+      num_iterated_edges = 0
+      selected = 0
+      for out_edge_id in G.GetNI(current_id).GetOutEdges():
+        num_iterated_edges = num_iterated_edges + 1
+        if out_edge_id in id_list_buffer:
+          continue;
+        if random.random() * (num_out_edges + 1 - num_iterated_edges) < (x - selected):
+          selected = selected + 1
+          id_list_buffer.append(out_edge_id)
+          tail = tail + 1
+
+      for new_id_pos in range(old_tail, tail):
+        new_G.AddNode(id_list_buffer[new_id_pos])
+        new_G.AddEdge(current_id, id_list_buffer[new_id_pos])
+
+      head = head + 1
+
+      if tail > num_nodes_G * density:
+        break;
+
+  return new_G
+
+#################################
+# directed
+
+def random_node_sample_graph_from_directed(G, density):
+  new_G = TNGraph.New()
+  for node_id in range(G.GetNodes()):
+    if random.random() < density:
+      new_G.AddNode(node_id)
+  
+  for edges in G.Edges():
+    src_id = edges.GetSrcNId()
+    dst_id = edges.GetDstNId()
+    if new_G.IsNode(src_id) and new_G.IsNode(dst_id):
+      new_G.AddEdge(src_id, dst_id)
+  
+  return new_G
+
+def random_edge_sample_graph_from_directed(G, density):
+  new_G = TNGraph.New()
+  for edges in G.Edges():
+    if random.random() < density:
+      src_id = edges.GetSrcNId()
+      dst_id = edges.GetDstNId()
+      if not new_G.IsNode(src_id):
+        new_G.AddNode(src_id)
+      if not new_G.IsNode(dst_id):
+        new_G.AddNode(dst_id)
+      new_G.AddEdge(src_id, dst_id)
+
+  return new_G
+
+def random_walking_sample_graph_from_directed(G, density, prob_jump_to_start = 0.15, max_iter_without_found = 100):
+  start_id_list = []
+
+  new_G = TNGraph.New()
+  num_nodes_new_G = 0
+  
+  num_nodes_G = G.GetNodes()
+  while num_nodes_new_G < num_nodes_G * density:
+    
+    # find the start point id
+    found = False
+    while not found:
+      start_id = G.GetRndNId()
+      if not start_id in start_id_list:
+        found = True
+    
+    # add new point to list
+    start_id_list.append(start_id)
+
+    # if not new_G.IsNode(start_id):
+    if not new_G.IsNode(start_id):
+      new_G.AddNode(start_id)
+      num_nodes_new_G = num_nodes_new_G + 1
+    
+    current_id = start_id
+    times_iter_without_found = 0
+    while num_nodes_new_G < num_nodes_G * density:
+      found = False
+
+      if random.random() < prob_jump_to_start:
+        current_id = start_id
+      
+      num_out_edges = sum(1 for x in G.GetNI(current_id).GetOutEdges())
+      num_iterated_edges = 0
+
+      last_id = current_id
+      for out_edge_id in G.GetNI(current_id).GetOutEdges():
+        if random.random() * (num_out_edges - num_iterated_edges) < 1:
+          current_id = out_edge_id
+          break;
+        num_iterated_edges = num_iterated_edges + 1
+
+      if not new_G.IsNode(current_id):
+        new_G.AddNode(current_id)
+        num_nodes_new_G = num_nodes_new_G + 1
+        found = True
+
+      if not new_G.IsEdge(last_id, current_id):
+        new_G.AddEdge(last_id, current_id)
+        found = True
+
+      if found:
+        times_iter_without_found = 0
+      else:
+        times_iter_without_found = times_iter_without_found + 1
+        if times_iter_without_found >= max_iter_without_found:
+          break
+
+  return new_G
+
+def forest_fire_sample_graph_from_directed(G, density, p = 0.2, r = 0.4):
+  id_list_buffer = []
+
+  new_G = TNGraph.New()
+  num_nodes_G = G.GetNodes()
+
+  head = 0
+  tail = 0
+
+  while tail < num_nodes_G * density:
+
+    # find the start point id
+    found = False
+    while not found:
+      start_id = G.GetRndNId()
+      if not start_id in id_list_buffer:
+        found = True
+
+    id_list_buffer.append(start_id)
+    tail = tail + 1
+
+    new_G.AddNode(start_id)
+
+    while head<tail:
+      current_id = id_list_buffer[head]
+      x = geometric(p=p, size=1)
+      y = geometric(p=r*p, size=1)
+
+      num_out_edges = sum(1 for x in G.GetNI(current_id).GetOutEdges())
+      num_in_edges = sum(1 for x in G.GetNI(current_id).GetInEdges())
+
+      old_tail = tail
+      num_iterated_edges = 0
+      selected = 0
+      for out_edge_id in G.GetNI(current_id).GetOutEdges():
+        num_iterated_edges = num_iterated_edges + 1
+        if out_edge_id in id_list_buffer:
+          continue;
+        if random.random() * (num_out_edges + 1 - num_iterated_edges) < (x - selected):
+          selected = selected + 1
+          id_list_buffer.append(out_edge_id)
+          tail = tail + 1
+
+      for new_id_pos in range(old_tail, tail):
+        new_G.AddNode(id_list_buffer[new_id_pos])
+        new_G.AddEdge(current_id, id_list_buffer[new_id_pos])
+
+      old_tail = tail
+      num_iterated_edges = 0
+      selected = 0
+      for in_edge_id in G.GetNI(current_id).GetInEdges():
+        num_iterated_edges = num_iterated_edges + 1
+        if in_edge_id in id_list_buffer:
+          continue;
+        if random.random() * (num_in_edges + 1 - num_iterated_edges) < (y - selected):
+          selected = selected + 1
+          id_list_buffer.append(in_edge_id)
+          tail = tail + 1
+
+      for new_id_pos in range(old_tail, tail):
+        new_G.AddNode(id_list_buffer[new_id_pos])
+        new_G.AddEdge(id_list_buffer[new_id_pos], current_id)
+
+      head = head + 1
+
+      if tail > num_nodes_G * density:
+        break;
+
+  return new_G
+
+############################################
+# original
 
 def get_full_graph_from_coo(coo):
   G = TNGraph.New()
@@ -59,7 +390,7 @@ def random_edge_sample_graph_from_full(G, density):
 
   return new_G
 
-def random_walking_sample_graph_from_full(G, density, prob_jump_to_start, max_iter_without_found = 100):
+def random_walking_sample_graph_from_full(G, density, prob_jump_to_start = 0.15, max_iter_without_found = 100):
   start_id_list = []
 
   new_G = TNGraph.New()
@@ -200,9 +531,22 @@ def main():
   # print G.GetNodes()
   # print G.GetEdges()
   # G = get_directed_graph_from_p2p_v1("source/p2p08/p2p-Gnutella08/p2p-Gnutella08.txt")
-  G = get_directed_graph_from_p2p_v1("source/p2p31/p2p-Gnutella31/p2p-Gnutella31.txt")
+  # G = get_directed_graph_from_p2p_v1("source/p2p31/p2p-Gnutella31/p2p-Gnutella31.txt")
+  G = get_undirected_graph_from_dblp_v1("source/com-DBLP/com-dblp.ungraph/com-dblp.ungraph.txt")
+  G1 = random_node_sample_graph_from_undirected(G, 0.2)
+  G2 = random_edge_sample_graph_from_undirected(G, 0.2)
+  G3 = random_walking_sample_graph_from_undirected(G, 0.2)
+  G4 = forest_fire_sample_graph_from_undirected(G, 0.2)
   print G.GetNodes()
   print G.GetEdges()
+  print G1.GetNodes()
+  print G1.GetEdges()
+  print G2.GetNodes()
+  print G2.GetEdges()
+  print G3.GetNodes()
+  print G3.GetEdges()
+  print G4.GetNodes()
+  print G4.GetEdges()
 
 if __name__ == '__main__':
   sys.exit(int(main() or 0))
--- process_graph.py
+++ process_graph.py
@@ -10,6 +10,103 @@ import collections
 # with warnings.catch_warnings():
 #   warnings.filterwarnings("ignore", category = DeprecationWarning)
 
+#############################
+# directed
+def node_degree_distribution_undirected(G):
+  num_nodes_G = G.GetNodes()
+
+  degrees = []
+  max_degree = 0
+  for node in G.Nodes():
+    degrees.append(node.GetOutDeg())
+    if degrees[-1]>max_degree:
+      max_degree=degrees[-1]
+  degrees_counter = collections.Counter(degrees)
+  degrees_frequency = [None]*(max_degree+1)
+  for i in range(max_degree+1):
+    degrees_frequency[i] = float(degrees_counter[i])/num_nodes_G
+
+  cumulative_frequency = degrees_frequency
+  for i in range(len(cumulative_frequency)-1):
+    cumulative_frequency[i+1] = cumulative_frequency[i+1] + cumulative_frequency[i]
+
+  return cumulative_frequency
+
+def clustering_coefficient_distribution_undirected(G, num_segments = 10):
+  coefficient = []
+  for node in G.Nodes():
+    nodes = TIntV()
+    for out_node_id in node.GetOutEdges():
+      nodes.Add(out_node_id)
+    subgraph = GetSubGraph(G, nodes)
+    d_v = len(nodes)
+    if d_v <= 1:
+      coefficient.append(0)
+    else:
+      coefficient.append(float(subgraph.GetEdges()) * 2/(d_v)/(d_v-1))
+  
+  coefficient = sorted(coefficient)
+  cumulative_frequency = [None]*(num_segments+1)
+  
+  pos = 0
+  for i in range(num_segments+1):
+    while (pos<len(coefficient) and coefficient[pos]<= 1./num_segments * i):
+      pos = pos + 1
+    cumulative_frequency[i] = pos
+
+  return [float(x)/pos for x in cumulative_frequency]
+
+#############################
+# undirected
+def node_degree_distribution_directed(G):
+  num_nodes_G = G.GetNodes()
+
+  degrees = []
+  max_degree = 0
+  for node in G.Nodes():
+    degrees.append(node.GetInDeg() + node.GetOutDeg())
+    if degrees[-1]>max_degree:
+      max_degree=degrees[-1]
+  degrees_counter = collections.Counter(degrees)
+  degrees_frequency = [None]*(max_degree+1)
+  for i in range(max_degree+1):
+    degrees_frequency[i] = float(degrees_counter[i])/num_nodes_G
+
+  cumulative_frequency = degrees_frequency
+  for i in range(len(cumulative_frequency)-1):
+    cumulative_frequency[i+1] = cumulative_frequency[i+1] + cumulative_frequency[i]
+
+  return cumulative_frequency
+
+def clustering_coefficient_distribution_directed(G, num_segments = 10):
+  coefficient = []
+  for node in G.Nodes():
+    nodes = TIntV()
+    for out_node_id in node.GetOutEdges():
+      nodes.Add(out_node_id)
+    for in_node_id in node.GetInEdges():
+      if not in_node_id in nodes:
+        nodes.Add(in_node_id)
+    subgraph = GetSubGraph(G, nodes)
+    d_v = len(nodes)
+    if d_v <= 1:
+      coefficient.append(0)
+    else:
+      coefficient.append(float(subgraph.GetEdges())/(d_v)/(d_v-1))
+  
+  coefficient = sorted(coefficient)
+  cumulative_frequency = [None]*(num_segments+1)
+  
+  pos = 0
+  for i in range(num_segments+1):
+    while (pos<len(coefficient) and coefficient[pos]<= 1./num_segments * i):
+      pos = pos + 1
+    cumulative_frequency[i] = pos
+
+  return [float(x)/pos for x in cumulative_frequency]
+
+#############################
+# original
 def node_degree_distribution(G):
   num_nodes_G = G.GetNodes()
 
@@ -30,7 +127,7 @@ def node_degree_distribution(G):
 
   return cumulative_frequency
 
-def clustering_coefficient_distribution(G, num_segments):
+def clustering_coefficient_distribution(G, num_segments = 10):
   coefficient = []
   for node in G.Nodes():
     nodes = TIntV()
@@ -44,7 +141,7 @@ def clustering_coefficient_distribution(G, num_segments):
     if d_v <= 1:
       coefficient.append(0)
     else:
-      coefficient.append(float(subgraph.GetNodes())/(d_v)/(d_v-1))
+      coefficient.append(float(subgraph.GetEdges())/(d_v)/(d_v-1))
   
   coefficient = sorted(coefficient)
   cumulative_frequency = [None]*(num_segments+1)
@@ -67,8 +164,6 @@ def generate_sample_collection_file(inputDir, outputDir, sampleNumber, methodCho
   jsonData1 = []
   jsonData2 = []
 
-  outputDir += str(sampleNumber)
-  outputDir += '_'
   outputDir += str(methodChoose)
   outputDir += '_'
 
@@ -82,7 +177,10 @@ def generate_sample_collection_file(inputDir, outputDir, sampleNumber, methodCho
     else:
       sampleGraph = forest_fire_sample_graph_from_full(G1, 0.1)
 
-    outputDir1 = outputDir + '1.json'   
+    outputDir1 = outputDir
+    outputDir1 += str(i + 1)
+    outputDir1 += '_'
+    outputDir1 += '1.json'   
     print 'file path & name: %s' % outputDir1   
     outputFile1 = open(outputDir1, 'w')
     # distribution = 1
@@ -92,7 +190,10 @@ def generate_sample_collection_file(inputDir, outputDir, sampleNumber, methodCho
     json.dump(jsonData1, outputFile1)
     outputFile1.close()
 
-    outputDir2 = outputDir + '2.json'      
+    outputDir2 = outputDir
+    outputDir2 += str(i + 1)
+    outputDir2 += '_'
+    outputDir2 += '2.json'     
     print 'file path & name: %s' % outputDir2
     outputFile2 = open(outputDir2, 'w')    
     # distribution = 2
@@ -135,15 +236,25 @@ def generate_sample_collection_file(inputDir, outputDir, sampleNumber, methodCho
   print '\n' 
 
 def main():
-  inputDir = 'source/amazon0312.mat'
-  inputTestDir = 'source/amazon0302.mat'
-  
-  outputDir = 'data/amazon/amazon'
+  G = get_undirected_graph_from_dblp_v1("source/com-DBLP/com-dblp.ungraph/com-dblp.ungraph.txt")
+  G1 = random_walking_sample_graph_from_undirected(G, 0.2)
+  print node_degree_distribution_undirected(G1)
+  print clustering_coefficient_distribution_undirected(G1)
 
-  sampleNumber = 3
-  methodChoose = 1
-  # distribution = 2
-  generate_sample_collection_file(inputTestDir, outputDir, sampleNumber, methodChoose)
+  # inputDir = 'source/amazon0312.mat'
+  # inputTestDir = 'source/amazon0302.mat'
+  
+  # outputDir = 'data/amazon/amazon'
+
+  # # sampleNumber = 3
+  # # methodChoose = 1
+  # # # distribution = 2
+  # # generate_sample_collection_file(inputTestDir, outputDir, sampleNumber, methodChoose)
+
+  # sampleNumber = 50
+  # for i in range(4):
+  #   methodChoose = i + 1
+  #   generate_sample_collection_file(inputDir, outputDir, sampleNumber, methodChoose)
 
   # time consuming
   # deprecated
